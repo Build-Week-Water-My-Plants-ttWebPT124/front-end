909848df7cdb7bd1cae6a50ba3224def1cb91553
GenHero <51133244+GenHero@users.noreply.github.com>
2021-03-25 16:06:27 -0400

    setup structure of Homepage/ added routes to Login and Signup pages

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
 import react from 'react'
+import { Route, Link } from 'react-router-dom'
+import Homepage from './components/Homepage';
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
+
   return (
     <div className="App">
       <nav className='navbar'>
-        <button>Login</button>
-        <button>Signup</button>
+        <Link><button>Login</button></Link>
+        <Link><button>Signup</button></Link>
       </nav>
 
       <div className='Site_Container'>
-        
+        <Route exact path='/'>
+          <Homepage />
+        </Route>
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route path='/signup'>
+          <Signup />
+        </Route>
 
       </div>
+
+      <footer className='footer'></footer>
     </div>
   );
 }
--- water-my-plants/src/components/Homepage.js
+++ water-my-plants/src/components/Homepage.js
@@ -1,12 +1,32 @@
 import react from 'react'
+import banner from '../assets/Banner.jfif'
+import feature1 from '../assets/feature-1.jpg';
+import feature2 from '../assets/feature-2.png';
+
 
 const Homepage = ()=>{
 
 
     return(
         <div>
+            <header>
+                <img src={banner} className='banner' />
+                <button>Get started</button>
+            </header>
            
-            
+            <div>
+                <div className='features'>
+                    <img src= {feature1} className='feature' />
+                    <h3>Feature 1</h3>
+                    <p>Feature 1 description will go here</p>
+                </div>
+
+                <div className='features'>
+                    <img src={feature2} className='feature'/>
+                    <h3>Feature 2</h3>
+                    <p>Feauture 2 description wil go here</p>
+                </div>
+            </div>
         </div>
     )
 }
