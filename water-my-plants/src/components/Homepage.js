46baebd4fc7d3a22189f022cef49195925c2cc13
GenHero <51133244+GenHero@users.noreply.github.com>
2021-03-25 16:12:44 -0400

    added placeholder text

--- water-my-plants/src/components/Homepage.js
+++ water-my-plants/src/components/Homepage.js
@@ -18,13 +18,13 @@ const Homepage = ()=>{
                 <div className='features'>
                     <img src= {feature1} className='feature' />
                     <h3>Feature 1</h3>
-                    <p>Feature 1 description will go here</p>
+                    <p>Feature 1 description will go here.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue. Lectus arcu bibendum at varius vel. At in tellus integer feugiat scelerisque varius morbi. Ultrices in iaculis nunc sed augue lacus viverra.</p>
                 </div>
 
                 <div className='features'>
                     <img src={feature2} className='feature'/>
                     <h3>Feature 2</h3>
-                    <p>Feauture 2 description wil go here</p>
+                    <p>Feauture 2 description wil go here.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue. Lectus arcu bibendum at varius vel. At in tellus integer feugiat scelerisque varius morbi. Ultrices in iaculis nunc sed augue lacus viverra.</p>
                 </div>
             </div>
         </div>
